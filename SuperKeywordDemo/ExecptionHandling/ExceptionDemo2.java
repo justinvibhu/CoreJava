a18caf2d0c7d0101f088cf3fb3debbeac897d191
vaibhav sontakke <justinvihu121@gmail.com>
2022-08-17 19:03:40 +0530

    Exception Demo 2

---
+++ SuperKeywordDemo/ExecptionHandling/ExceptionDemo2.java
@@ -0,0 +1,47 @@
+package ExecptionHandling;
+
+import javax.crypto.AEADBadTagException;
+import java.lang.reflect.Parameter;
+import java.util.InputMismatchException;
+import java.util.MissingFormatArgumentException;
+import java.util.Scanner;
+import java.util.concurrent.LinkedBlockingDeque;
+
+public class ExceptionDemo2 {
+    static int a[] =new int[10];
+    public static void main(String[] args) {
+
+
+        try {
+            reaArray();
+        }
+        catch (InputMismatchException e){
+            System.out.println("Please enter the Elements:");
+            try {
+                reaArray();
+            }
+            catch (ArrayIndexOutOfBoundsException e){
+                System.out.println("IndexOutOfBound Exception");
+
+            }
+        }
+        catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("IndexOutOfBound Exception");
+        }
+    }
+
+    private static void reaArray() {
+        System.out.println("Enter the element in array");
+        int i=0;
+        while(true){
+            if(a[i]!=(int) a[i]){
+                throw new InputMismatchException();
+            }
+            if(i>a.length-1){
+                throw new ArrayIndexOutOfBoundsException();
+            }
+            Scanner sc =new Scanner(System.in);
+            a[i]=sc.nextInt();
+        }
+    }
+}
