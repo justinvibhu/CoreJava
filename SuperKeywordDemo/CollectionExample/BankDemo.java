18efdefcf5c4fe1ce2a032a80fc854725657747a
vaibhav sontakke <justinvihu121@gmail.com>
2022-08-24 23:44:11 +0530

    CollectionExample With ArrayList

--- SuperKeywordDemo/CollectionExample/BankDemo.java
+++ SuperKeywordDemo/CollectionExample/BankDemo.java
@@ -4,14 +4,14 @@ import java.util.ArrayList;
 
 public class BankDemo {
     public static void main(String[] args) {
-        ArrayList<Account> acclist=new ArrayList<>();
+        ArrayList<Account> accountList=new ArrayList<>();
         Account a1=new Account(101,5000,32440);
         Account a2=new Account(102,3000,48383);
         Account a3=new Account(103,2000,78937);
-        acclist.add(a1);
-        acclist.add(a2);
-        acclist.add(a3);
-        Bank b=new Bank(acclist);
+        accountList.add(a1);
+        accountList.add(a2);
+        accountList.add(a3);
+        Bank b=new Bank(accountList);
         b.Deposite(25000,32440);
         b.Withdraw(5000,32440);
 
